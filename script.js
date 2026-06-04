f9a80601a28835adb588b4cc9ed6690ed7a2a11e
1JETSKI1 <261773881+1JETSKI1@users.noreply.github.com>
2026-06-04 19:02:43 +0200

    add playRound function

--- script.js
+++ script.js
@@ -22,17 +22,51 @@ function getComputerChoice () {
     }
 }
 
-console.log(getComputerChoice());
-
 //function getHumanChoice 
 //ask for human to type rock, paper or scissors
 //return their choice
-function getHumanChoice () {
+
+function getHumanChoice() {
     let humanChoice = prompt("Choose rock,paper or scissors");
     return humanChoice;
 }
 
-console.log(getHumanChoice())
-
+//add humanScore 
+//add comuterScore
 let humanScore = 0
 let computerScore = 0
+
+//function playRound
+//if human has rock and computer has scissors → human wins
+//if human has scissors and computer has paper → human wins
+//if human has paper and computer has rock → human wins
+//otherwise if both choices are the same → it's a tie
+//otherwise → computer wins
+
+function playRound (humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++; 
+    console.log("You win! rock beats scissors");
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++; 
+        console.log("You win! scissors beats paper");
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++; 
+        console.log("You win! paper beats rock");
+    }
+
+    else if (humanChoice === computerChoice) {
+        console.log("Its's a tie!");
+    }
+    else {
+        computerScore++;
+        console.log("You Lose!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
