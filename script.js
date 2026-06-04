7b7571b5a9f14d24fa20110e84deb0592061e89e
1JETSKI1 <261773881+1JETSKI1@users.noreply.github.com>
2026-06-04 17:56:40 +0200

    add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("Hello World")
+
+//function getComputerChoice
+// generate a random number
+// if number < 0.33 give rock
+// esle if number < 0.66 give paper
+// otherwise give scissors
+
+function getComputerChoice () {
+    let randomNumber = Math.random();
+    
+    if (randomNumber < 0.33) {
+        return "rock";
+    }
+    
+    else if (randomNumber < 0.66) {
+        return "paper";
+    }
+
+    else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
